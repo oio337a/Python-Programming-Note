d2bca1ba8e1dcee81fd43ebd4bbfa2a0f3ac77f0
Yongmin Park <yongmipa@student.42seoul.kr>
2023-02-16 16:27:40 +0900

    add insertion sort

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,12 @@
+# 삽입 정렬
+
+test_array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(1, len(test_array)):
+	for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복
+		if test_array[j] < test_array[j - 1]: # 한 칸씩 왼쪽으로 이동
+			test_array[j], test_array[j - 1] = test_array[j - 1], test_array[j] # 스왑
+		else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+			break
+
+print(test_array)
