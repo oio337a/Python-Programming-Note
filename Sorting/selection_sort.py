eb198a3e00a75d83af9c0cd76fad40aa26faf42f
Yongmin Park <yongmipa@student.42seoul.kr>
2023-02-16 16:15:41 +0900

    add selection sort

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,12 @@
+# 선택 정렬
+
+test_array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(test_array)):
+	min_index = 1 # 가장 작은 원소의 인덱스
+	for j in range(i + 1, len(test_array)):
+		if test_array[min_index] > test_array[j]:
+			min_index = j
+	test_array[i], test_array[min_index] = test_array[min_index], test_array[i] # 스왑
+
+print(test_array)
